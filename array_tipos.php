7d14d48357d5cc9025cd000fc13ba7dfa90eba25
daniel <daniel.aguero@xl.com.ar>
2021-06-02 20:11:04 -0300

    array tipos modificado foreach

--- array_tipos.php
+++ array_tipos.php
@@ -5,20 +5,24 @@ $bd_tipo=new bd();
 $query2=$bd_tipo->connect()->prepare('SELECT id_tipo,nombre FROM tipo');
 $query2->execute();
 $tipos=array();
+$b=0;
 while($row=$query2->fetch(PDO::FETCH_ASSOC))
 {
    $tipos[$row['id_tipo']]=$row['nombre'];
+
 }
 
 return json_encode($tipos);
 function dibujarSelect()
 {
     global $tipos;
-    $c=1;
-    while($c<=count($tipos))
-    {
-            echo '<option value="'.$c.'">'.$tipos[$c].'</option>';
-            $c++;
-    }
+
+    foreach($tipos as $key=>$value)
+        {
+            if($key!='')
+            {
+                 echo '<option value="'.$key.'">'.$value.'</option>';
+            }
+        }
 }
 ?>
